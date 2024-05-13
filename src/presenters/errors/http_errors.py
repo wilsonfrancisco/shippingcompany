2552c8d18e6062f6ea827f3083f1ce0bbb754864
wilsonfrancisco <wilson.gs.francisco@gmail.com>
2024-05-14 00:30:43 +0100

    feat: FindPeopleController implementation

---
+++ src/presenters/errors/http_errors.py
@@ -0,0 +1,12 @@
+class HttpErrors:
+    """Http errors definition"""
+
+    @staticmethod
+    def error_422():
+        """HTTP Code: 422 - Unprocessable Entity"""
+        return {"status_code": 422, "body": {"error": "Unprocessable Entity"}}
+
+    @staticmethod
+    def error_400():
+        """HTTP Code: 400 - Bad Request"""
+        return {"status_code": 400, "body": {"error": "Bad Request"}}
